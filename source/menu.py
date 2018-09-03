8c47d0935917b8561830998f92cab4f906ce109e
Jacob/Otis Root <otisdodo@gmail.com>
2018-09-02 17:43:35 -0700

    Started work

--- source/menu.py
+++ source/menu.py
@@ -1,3 +1,9 @@
 class menu:
     def __init__():
+        try:
+            self.save = file.open("save.txt",'a')
+            self.save.close()
+            self.fileexists = True
+        except:
+            pass
         
