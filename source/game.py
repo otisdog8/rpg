835ce5f77bf895b91b469613bb880ef52e682701
Jacob/Otis Root <otisdodo@gmail.com>
2018-09-03 16:07:32 -0700

    Redid classes

--- source/game.py
+++ source/game.py
@@ -1,4 +1,15 @@
-class game:
-    def __init__():
+class game(object):
+    """Starts the game, coordinates menus, fighting, turns, ect, ect"""
+    def __init__(self, arg):
+        super(game, self).__init__()
+        self.arg = arg
         import menu
-        self.menu = menu()
+        #Check for existence of savefile
+        try:
+            #open savefile
+            self.savefileexists = True
+        except:
+            self.savefileexists = False
+        self.start(savefileexists)
+    def start(savefileexists):
+        pass
--- source/menu.py
+++
@@ -1,8 +0,0 @@
-class menu:
-    def __init__():
-        try:
-            self.save = file.open("save.txt",'a')
-            self.save.close()
-            self.fileexists = True
-        except:
-            pass
