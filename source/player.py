0e71a2c3a76044cabd8c8a645bb2d0a2e3336709
Jacob/Otis Root <otisdodo@gmail.com>
2018-09-03 17:26:24 -0700

    Commiting for backups

--- source/player.py
+++ source/player.py
@@ -1,5 +1,8 @@
+#This allows my custom import method to find the class
+def import():
+    return player()
 class player():
     """The main player class; handles things like stats, and is one of the classes involved with fighting."""
-    def __init__(self, arg):
-        self.arg = arg
-  
+    def __init__(self):
+        pass
+    
