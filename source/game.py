c39b49947245fd0fd5e80f6546feb92ccc88f6ac
Jacob/Otis Root <otisdodo@gmail.com>
2018-09-03 18:19:10 -0700

    More stat enhancements

--- source/game.py
+++ source/game.py
@@ -2,22 +2,23 @@ class game:
     """Starts the game, coordinates menus, fighting, turns, ect, ect"""
     def __init__(self):
         self.savemanager = self.importer('savemanagement')
+        self.player = self.importer('player')
         try:
             savefile = open("savefile.txt",'r')
             savefile.close()
-            self.savefileexists = True
+            savefileexists = True
         except:
-            self.savefileexists = False
-        self.start(self.savefileexists)
+            savefileexists = False
+        self.start(savefileexists)
 
     def start(self,savefileexists):
         if savefileexists == False:
             self.save = self.savemanager.nosave()
-            print(self.save)
         elif savefileexists == True:
             self.save = self.savemanager.opensave()
         else:
             print("This is am impossible error")
+        self.player.getstats(self.save)
     def importer(self,module):
         output = __import__(module)
         return output.add()
--- source/player.py
+++ source/player.py
@@ -1,7 +1,17 @@
 #This allows my custom import method to find the class
-def import():
+def add():
     return player()
 class player():
     """The main player class; handles things like stats, and is one of the classes involved with fighting."""
     def __init__(self):
-        pass
+        import math
+        self.math = math
+    def getstats(self,stats): #This gives me all the stats I need to live a happy, fuffilling life
+        self.endurance = stats[0]
+        self.strength = stats[1]
+        self.xp = stats[2]
+        self.hp = stats[3]
+        self.maxhp = self.endurance * 10
+        self.level = self.math.floor(float(self.xp/100))
+        self.totalstats = self.level*5+20
+    
